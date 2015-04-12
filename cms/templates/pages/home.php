57e54c0c1a5ad040fa117a8727b7fc792ebf9446
Martin Underhill <martin@tempertemper.net>
2015-04-12 15:52:10 +0100

    adds blog posts to homepage

--- cms/templates/pages/home.php
+++ cms/templates/pages/home.php
@@ -4,12 +4,12 @@
     'body-class' => 'home',
   ]);
 
-  perch_content('Main heading');
-
-  perch_pages_navigation([
-    'levels'=>1
-  ]);
-
   perch_content('Primary content');
+
+  perch_blog_recent_posts(10);
+
+  perch_blog_categories();
+  perch_blog_tags();
+  perch_blog_date_archive_years();
 
   perch_layout('global/footer');
