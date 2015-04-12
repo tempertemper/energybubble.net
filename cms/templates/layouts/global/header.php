7bb989543ddb37e8a0642e1455906c01d7a6304d
Martin Underhill <martin@tempertemper.net>
2015-04-12 11:32:23 +0100

    tidies formatting of default page header include

--- cms/templates/layouts/global/header.php
+++ cms/templates/layouts/global/header.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
+  <meta charset="utf-8" />
 	<title><?php perch_pages_title(); ?></title>
 	<?php perch_page_attributes(); ?>
 	<?php perch_get_css(); ?>
 </head>
-<?php 
+<?php
 	if (perch_layout_has('body-class')) {
 		echo '<body class="'.perch_layout_var('body-class', true).'">';
 	}else{
