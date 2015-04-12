d3913f25445225d2c7121a9c89a920dad0475814
Martin Underhill <martin@tempertemper.net>
2015-04-12 15:50:32 +0100

    moves main navigation to footer

--- cms/templates/layouts/global/footer.php
+++ cms/templates/layouts/global/footer.php
@@ -1,4 +1,15 @@
   <footer role="contentinfo">
+
+    <nav role="navigation">
+      <?php
+        perch_pages_navigation([
+          'hide-extensions'=>true,
+          'hide-default-doc'=>true,
+          'levels'=>1,
+        ]);
+      ?>
+    </nav>
+
     <p>Site designed and built by <a href="https://tempertemper.net">tempertemper Web Design</a></p>
     <small>Copyright &copy; <?php echo date('Y'); ?></small>
   </footer>
