074f7d7e34c1cc7bb4e26939c128136bdc2649d5
Martin Underhill <martin@tempertemper.net>
2015-04-12 11:16:55 +0100

    default runway page templates

--- cms/templates/pages/default.php
+++ cms/templates/pages/default.php
@@ -1,10 +1,19 @@
-<?php
+<?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+	<title><?php perch_pages_title(); ?></title>
+	<?php perch_page_attributes(); ?>
+	<?php perch_get_css(); ?>
+</head>
+<body>
+    <h1><?php perch_content('Main heading'); ?></h1>
 
-  // Header
-  perch_layout('global/head');
+    <?php perch_pages_navigation(array('levels'=>1)); ?>
 
-  // Page content
-  perch_content('Primary content');
+    <?php perch_content('Intro'); ?>
 
-  // Footer
-  perch_layout('global/footer');
+    <?php perch_get_javascript(); ?>
+</body>
+</html>
