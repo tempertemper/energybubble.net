2990d6f035eaf0895ee0b15ee5620aae3fe8483f
Martin Underhill <martin@tempertemper.net>
2015-04-12 14:18:15 +0100

    fleshes footer out a little

--- cms/templates/layouts/global/footer.php
+++ cms/templates/layouts/global/footer.php
@@ -1,3 +1,9 @@
-    <?php perch_get_javascript(); ?>
+  <footer role="contentinfo">
+    <p>Site designed and built by <a href="https://tempertemper.net">tempertemper Web Design</a></p>
+    <small>Copyright &copy; <?php echo date('Y'); ?></small>
+  </footer>
+
+  <?php perch_get_javascript(); ?>
+
 </body>
 </html>
